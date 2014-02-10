6e8b05117607ff683515600cb17792c9ee59ef03
Matthew Miner <matthew@matthewminer.com>
2014-02-10 12:38:43 -0500

    Initial commit.

---
+++ Assets/Hierarchy Icons/Editor/IconInserter.cs
@@ -0,0 +1,110 @@
+//
+// Author: Matthew Miner
+//         matthew@matthewminer.com
+//         http://matthewminer.com
+//
+// Copyright (c) 2014
+//
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+namespace HierarchyIcons
+{
+	/// <summary>
+	/// Adds applicable icons next to game objects in the hierarchy.
+	/// </summary>
+	[InitializeOnLoad]
+	static class IconInserter
+	{
+		static readonly Dictionary<Type, char> componentIcons = new Dictionary<Type, char>()
+		{
+			{ typeof(Animation),      '>' },
+			{ typeof(AudioListener),  'A' },
+			{ typeof(AudioSource),    'a' },
+			{ typeof(Camera),         'c' },
+			{ typeof(Cloth),          'C' },
+			{ typeof(ConstantForce),  'f' },
+			{ typeof(GUIText),        'g' },
+			{ typeof(GUITexture),     'G' },
+			{ typeof(Light),          'l' },
+			{ typeof(NetworkView),    'n' },
+			{ typeof(ParticleSystem), 'p' },
+			{ typeof(Projector),      'P' },
+			{ typeof(Rigidbody),      'r' },
+			{ typeof(Terrain),        't' },
+			{ typeof(Tree),           'T' },
+		};
+
+		static readonly Dictionary<string, char> tagIcons = new Dictionary<string, char>()
+		{
+			{ "Player", 'p' }
+		};
+
+		const string fontPath = "Assets/Hierarchy Icons/Icons.ttf";
+		static Font font;
+
+		static GUIStyle _labelStyle;
+		static GUIStyle labelStyle
+		{
+			get {
+				if (_labelStyle == null) {
+					_labelStyle = new GUIStyle(EditorStyles.label);
+					_labelStyle.font = font;
+					_labelStyle.alignment = TextAnchor.MiddleRight;
+				}
+
+				return _labelStyle;
+			}
+		}
+
+		static IconInserter ()
+		{
+			font = AssetDatabase.LoadAssetAtPath(fontPath, typeof(Font)) as Font;
+
+			if (font == null) {
+				Debug.LogError("[Icon Hierarchy] The font file appears to be missing. Perhaps it was moved?");
+			} else {
+				EditorApplication.hierarchyWindowItemOnGUI += HighlightItems;
+			}
+		}
+
+		/// <summary>
+		/// Displays icons beside each game object in the Hierarchy panel.
+		/// </summary>
+		/// <param name="instanceID">ID of object.</param>
+		/// <param name="selectionRect">Boundaries of object's label.</param>
+		static void HighlightItems (int instanceID, Rect selectionRect)
+		{
+			var target = EditorUtility.InstanceIDToObject(instanceID) as GameObject;
+			var iconString = GetIconString(target);
+			GUI.Label(selectionRect, iconString, labelStyle);
+		}
+
+		/// <summary>
+		/// Gets a string of characters mapping to icons for the game object.
+		/// </summary>
+		/// <param name="target">Target game object.</param>
+		/// <returns>Icon string.</returns>
+		static string GetIconString (GameObject target)
+		{
+			var icons = target.GetComponents<Component>()
+				.Select(component => component.GetType())
+				.Where(type => componentIcons.ContainsKey(type))
+				.Select(type => componentIcons[type])
+				.Distinct()
+				.ToList();
+
+			// Add icon for tag.
+			if (target.tag != null && tagIcons.ContainsKey(target.tag)) {
+				icons.Add(tagIcons[target.tag]);
+			}
+
+			var iconString = string.Join(" ", icons.Select(c => c.ToString()).ToArray());
+			return iconString;
+		}
+	}
+}
